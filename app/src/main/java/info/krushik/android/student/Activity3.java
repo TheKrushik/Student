4fecc81f88db08dcfeff736b5004878fded9af21
krush <krushik@ukr.net>
2016-05-11 17:00:37 +0300

    notification save

--- app/src/main/java/info/krushik/android/student/Activity3.java
+++ app/src/main/java/info/krushik/android/student/Activity3.java
@@ -1,6 +1,11 @@
 package info.krushik.android.student;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,8 +48,27 @@ public class Activity3 extends AppCompatActivity {
                 resultIntent.putExtra(MainActivity.EXTRA_LASTNAME, mEditTextLastName.getText().toString());
                 resultIntent.putExtra(MainActivity.EXTRA_AGE, mEditTextAge.getText().toString());
                 setResult(RESULT_OK, resultIntent);
-                Toast.makeText(Activity3.this, R.string.toast_save, Toast.LENGTH_SHORT).show();
                 finish();
+
+                Toast.makeText(Activity3.this, R.string.toast_save, Toast.LENGTH_SHORT).show();
+
+                Intent intentNotification = new Intent(Activity3.this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(Activity3.this, 0, intentNotification, 0);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                Notification notification = new NotificationCompat.Builder(Activity3.this)
+                        .setAutoCancel(true)
+                        .setSmallIcon(R.drawable.about)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setTicker(getString(R.string.student_editing))
+                        .setContentTitle(getString(R.string.student_edit))
+                        .setContentText(getString(R.string.student_save))
+                        .setWhen(System.currentTimeMillis())
+                        .setContentIntent(pendingIntent)
+                        .build();
+                notificationManager.notify(1, notification);
+
 
             }
 
